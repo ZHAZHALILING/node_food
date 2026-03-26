7a2c210c35cac2a0af6f62e30716384fadb604f2
zmh <1041406370@qq.com>
2026-03-26 10:44:56 +0800

    测试

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var cartRouter = require('./routes/cart/cart');
-var dishesRouter = require('./routes/dishes/dishes');
+// var cartRouter = require('./routes/cart/cart');
+// var dishesRouter = require('./routes/dishes/dishes');
 var usersRouter = require('./routes/users/users');
-var mealPlansRouter = require('./routes/meal_plans/index');
-var uploadRouter = require('./routes/upload');
+// var mealPlansRouter = require('./routes/meal_plans/index');
+// var uploadRouter = require('./routes/upload');
 var app = express();
 
 
@@ -20,8 +20,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/dishes', dishesRouter);
-app.use('/api/plans', mealPlansRouter);
-app.use('/api/upload', uploadRouter);
+// app.use('/api/cart', cartRouter);
+// app.use('/api/dishes', dishesRouter);
+// app.use('/api/plans', mealPlansRouter);
+// app.use('/api/upload', uploadRouter);
 module.exports = app;
